99838fb34119f1c64050e78e351e2e8c0035b50e
ZibaloV <fotoroom.md@gmail.com>
2019-04-30 11:21:36 +0300

    сетка готова

--- smartgrid.js
+++ smartgrid.js
@@ -1,20 +1,21 @@
 module.exports = {
 	columns:12,
-	offset: "10px",
+	offset: "16px",
 	container: {
-		maxWidth: "1160px",
-		fields: "30px"
+		maxWidth: "1024px",
+		fields: "32px"
 	},
 	breakPoints: {
 		md: {
-            width: "992px",
-            fields: "15px"
+            width: "960px",
+            
         },
         sm: {
-            width: "720px"
+            width: "720px",
+            fields: "16px"
         },
         xs: {
-            width: "576px",
+            width: "540px",
             /* offset: "10px",
             fields: "5px" */
 	    },
